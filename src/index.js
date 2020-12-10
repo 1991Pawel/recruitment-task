b20003d78afceb2ee84e0ed3edb1581788e08ed8
Pawel L <virtuozoo@o2.pl>
2020-12-10 04:26:04 +0100

    add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './styles/main.scss';
+
+import Swiper, { Navigation, Autoplay } from 'swiper';
+
+Swiper.use([Autoplay, Navigation]);
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+swiper.autoplay.start();
+
